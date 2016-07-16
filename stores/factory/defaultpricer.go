c8e70f6eee64ca141f433cd89756628f30209b4b
Virgile Quintin <virgile.quintin@gmail.com>
2016-07-16 19:55:49 +0200

    Solve a bug related to the index pricer calling the cache.

--- stores/factory/defaultpricer.go
+++ stores/factory/defaultpricer.go
@@ -12,11 +12,12 @@ import (
 // NewPricer makes a default pricer for Currency, Equity and Index, with caching.
 func NewPricer() stores.Pricer {
 	store1 := stores.FailPricer{}
-	store2 := currency.CurrencyPricer{Next: store1}
-	store3 := equity.YahooPricer{Next: store2}
+	store2 := currency.CurrencyPricer{Next: &store1}
+	store3 := equity.YahooPricer{Next: &store2}
 	rateStore1 := fx.FixerChanger{}
-	rateStore2 := cache.NewExchangeRateCache(rateStore1)
-	store4 := cache.NewPricerCache(store3)
-	store5 := index.IndexPricer{Next: &store4, Head: &store4, Changer: &rateStore2}
+	rateStore2 := cache.NewExchangeRateCache(&rateStore1)
+	store4 := index.IndexPricer{Next: &store3, Head: &store1, Changer: &rateStore2}
+	store5 := cache.NewPricerCache(&store4)
+	store4.Head = &store5
 	return &store5
 }
